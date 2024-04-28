fccfe5c2e9eb1885341ac46ec0a70d612db04a78
Vasco Castro <vascocastro@student.dei.uc.pt>
2024-04-28 20:35:10 +0100

    v1

--- src/main/java/paj/project5_vc/service/DashboardService.java
+++ src/main/java/paj/project5_vc/service/DashboardService.java
@@ -31,7 +31,7 @@ public class DashboardService {
         String userCounts = userBean.getUsersCount(token);
         if (userCounts.isEmpty()) {
             // Handle the case where the userCounts is empty
-            return Response.status(403).entity(userCounts).build();
+            return Response.status(403).entity("Invalid token").build();
         }
         return Response.status(200).entity(userCounts).build();
     }
@@ -47,7 +47,7 @@ public class DashboardService {
         List<TasksSummary> stateTasks = taskBean.countTasksByStatus(token);
         if (stateTasks.isEmpty()) {
             // Handle the case where List<TasksSummary> is empty
-            return Response.status(403).entity(stateTasks).build();
+            return Response.status(403).entity("Invalid token").build();
         }
         return Response.status(200).entity(stateTasks).build();
     }
@@ -63,7 +63,7 @@ public class DashboardService {
         List<TasksSummary> categoryTasks = taskBean.getCategoryTasksBySum(token);
         if (categoryTasks.isEmpty()) {
             // Handle the case where List<TasksSummary> is empty
-            return Response.status(403).entity(categoryTasks).build();
+            return Response.status(403).entity("Invalid token").build();
         }
         return Response.status(200).entity(categoryTasks).build();
     }
@@ -107,7 +107,7 @@ public class DashboardService {
         List<DayCount> validatedUsersCountForLastWeek = userBean.getRegisteredUsersCountForLastWeek();
         if (validatedUsersCountForLastWeek.isEmpty()) {
             // Handle the case where List<DayCount> is empty
-            return Response.status(403).entity(validatedUsersCountForLastWeek).build();
+            return Response.status(403).entity("Invalid token").build();
         }
         return Response.status(200).entity(validatedUsersCountForLastWeek).build();
     }
@@ -123,7 +123,7 @@ public class DashboardService {
         List<DayCount> result = taskBean.getCompletedTasksCumulative();
         if (result.isEmpty()) {
             // Handle the case where List<DayCount> is empty
-            return Response.status(403).entity(result).build();
+            return Response.status(403).entity("Invalid token").build();
         }
         return Response.status(200).entity(result).build();
     }
